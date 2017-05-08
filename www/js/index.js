ce117003f5ba7563c4b99c9d0bdfbc2715b0b2cf
ssimilae <ssimilae@gmail.com>
2017-05-08 14:29:10 +0900

    1
    2

--- www/js/index.js
+++ www/js/index.js
@@ -106,13 +106,13 @@ var app = {
 				if(data.wasTapped){
 				  //Notification was received on device tray and tapped by the user.
 				  
-				  //alert( JSON.stringify(data) );
+					alert( JSON.stringify(data) );
 					var push =  JSON.stringify(data);
 					alert(push.body+".");
 				}else{
 				  //Notification was received in foreground. Maybe the user needs to be notified.
 			
-				  //alert( JSON.stringify(data) );
+				  alert( JSON.stringify(data) );
 				  var push =  JSON.stringify(data);
 					alert(push.body+"..");
 				}
