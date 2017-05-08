3122f372d20b1f58c85cd0ec7f04dfcf253476ed
ssimilae <ssimilae@gmail.com>
2017-05-08 13:08:21 +0900

    1
    2

--- www/js/index.js
+++ www/js/index.js
@@ -107,10 +107,14 @@ var app = {
 				  //Notification was received on device tray and tapped by the user.
 				  
 				  //alert( JSON.stringify(data) );
+					var push =  JSON.stringify(data);
+					alert(push.body+".");
 				}else{
 				  //Notification was received in foreground. Maybe the user needs to be notified.
 			
 				  //alert( JSON.stringify(data) );
+				  var push =  JSON.stringify(data);
+					alert(push.body+"..");
 				}
 			});
 			
