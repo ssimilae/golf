a7faf752d2f9175b2a7154eb2d0a635a31701e4b
ssimilae <ssimilae@gmail.com>
2017-05-22 11:00:11 +0900

    1
    2

--- www/js/index.js
+++ www/js/index.js
@@ -32,6 +32,7 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
 		try
 		{
